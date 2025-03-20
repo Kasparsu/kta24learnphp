8d92539d5a6d91e8bd59ed4bc3d86400ec4dab77
Kaspar Martin Suursalu <kasparsu@gmail.com>
2025-03-20 16:02:04 +0200

    Add autoloading example

--- public/index.php
+++ public/index.php
@@ -4,28 +4,41 @@ if (preg_match('/\.(?:png|jpg|jpeg|gif|css|js)$/', $_SERVER["REQUEST_URI"])) {
     return false;    // serve the requested resource as-is.
 }
 
+spl_autoload_register(function ($class) {
+    $parts = explode('\\', $class);
+    array_shift($parts);
+    $class = implode($parts);
+    require_once "src/$class.php";
+});
 
-switch($_SERVER['REQUEST_URI']){
-    case '/':
-        $heroTitle = 'World news';
-        $posts = [
-            ['title' => 'World news 1', 'author' => 'Kaspar', 'body' => 'some content here 1'],
-            ['title' => 'World news 2', 'author' => 'Martin', 'body' => 'some content here 2'],
-            ['title' => 'World news 3', 'author' => 'Peter', 'body' => 'some content here 3'],
-            ['title' => 'World news 4', 'author' => 'Pets', 'body' => 'some content here 4'],
-        ];
-        include 'views/index.php';
-        break;
-    case '/us':
-        $heroTitle = 'U.S news';
-        $posts = [
-            ['title' => 'U.S news 1', 'author' => 'Lauri', 'body' => 'some content here 1'],
-            ['title' => 'U.S news 2', 'author' => 'Jaanika', 'body' => 'some content here 2'],
-            ['title' => 'U.S news 3', 'author' => 'Ethel', 'body' => 'some content here 3'],
-            ['title' => 'U.S news 4', 'author' => 'Kaur', 'body' => 'some content here 4'],
-        ];
-        include 'views/us.php';
-        break;
-    default:
-        echo '404';
-}   
+// autoloading not explained fully (folders), Namespaces explain!
+
+$router = new App\Router();
+$db = new App\DB();
+var_dump($router, $db);
+
+
+// switch($_SERVER['REQUEST_URI']){
+//     case '/':
+//         $heroTitle = 'World news';
+//         $posts = [
+//             ['title' => 'World news 1', 'author' => 'Kaspar', 'body' => 'some content here 1'],
+//             ['title' => 'World news 2', 'author' => 'Martin', 'body' => 'some content here 2'],
+//             ['title' => 'World news 3', 'author' => 'Peter', 'body' => 'some content here 3'],
+//             ['title' => 'World news 4', 'author' => 'Pets', 'body' => 'some content here 4'],
+//         ];
+//         include 'views/index.php';
+//         break;
+//     case '/us':
+//         $heroTitle = 'U.S news';
+//         $posts = [
+//             ['title' => 'U.S news 1', 'author' => 'Lauri', 'body' => 'some content here 1'],
+//             ['title' => 'U.S news 2', 'author' => 'Jaanika', 'body' => 'some content here 2'],
+//             ['title' => 'U.S news 3', 'author' => 'Ethel', 'body' => 'some content here 3'],
+//             ['title' => 'U.S news 4', 'author' => 'Kaur', 'body' => 'some content here 4'],
+//         ];
+//         include 'views/us.php';
+//         break;
+//     default:
+//         echo '404';
+// }   

3efeb5c9497a58d09ad9db8ca561f824bef252e2
Kaspar Martin Suursalu <kasparsu@gmail.com>
2025-03-20 15:38:21 +0200

    Add modern single entry php example

---
+++ public/index.php
@@ -0,0 +1,31 @@
+<?php
+
+if (preg_match('/\.(?:png|jpg|jpeg|gif|css|js)$/', $_SERVER["REQUEST_URI"])) {
+    return false;    // serve the requested resource as-is.
+}
+
+
+switch($_SERVER['REQUEST_URI']){
+    case '/':
+        $heroTitle = 'World news';
+        $posts = [
+            ['title' => 'World news 1', 'author' => 'Kaspar', 'body' => 'some content here 1'],
+            ['title' => 'World news 2', 'author' => 'Martin', 'body' => 'some content here 2'],
+            ['title' => 'World news 3', 'author' => 'Peter', 'body' => 'some content here 3'],
+            ['title' => 'World news 4', 'author' => 'Pets', 'body' => 'some content here 4'],
+        ];
+        include 'views/index.php';
+        break;
+    case '/us':
+        $heroTitle = 'U.S news';
+        $posts = [
+            ['title' => 'U.S news 1', 'author' => 'Lauri', 'body' => 'some content here 1'],
+            ['title' => 'U.S news 2', 'author' => 'Jaanika', 'body' => 'some content here 2'],
+            ['title' => 'U.S news 3', 'author' => 'Ethel', 'body' => 'some content here 3'],
+            ['title' => 'U.S news 4', 'author' => 'Kaur', 'body' => 'some content here 4'],
+        ];
+        include 'views/us.php';
+        break;
+    default:
+        echo '404';
+}   

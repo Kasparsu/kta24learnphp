410ca3c224be3024a2ddecaa3523c32079b30fc5
Kaspar Martin Suursalu <kasparsu@gmail.com>
2025-04-03 09:39:54 +0300

    Add better router

--- public/index.php
+++ public/index.php
@@ -11,37 +11,22 @@ spl_autoload_register(function ($class) {
     require_once "src/$class.php";
 });
 
+require 'helpers.php';
+require 'routes.php';
 
-use App\Controllers\Router as CRouter;
-use App\Router;
-
-$router = new Router();
-$router2 = new CRouter();
-$db = new App\DB();
-var_dump($router,$router2, $db);
-
-
-// switch($_SERVER['REQUEST_URI']){
-//     case '/':
-//         $heroTitle = 'World news';
-//         $posts = [
-//             ['title' => 'World news 1', 'author' => 'Kaspar', 'body' => 'some content here 1'],
-//             ['title' => 'World news 2', 'author' => 'Martin', 'body' => 'some content here 2'],
-//             ['title' => 'World news 3', 'author' => 'Peter', 'body' => 'some content here 3'],
-//             ['title' => 'World news 4', 'author' => 'Pets', 'body' => 'some content here 4'],
-//         ];
-//         include 'views/index.php';
-//         break;
-//     case '/us':
-//         $heroTitle = 'U.S news';
-//         $posts = [
-//             ['title' => 'U.S news 1', 'author' => 'Lauri', 'body' => 'some content here 1'],
-//             ['title' => 'U.S news 2', 'author' => 'Jaanika', 'body' => 'some content here 2'],
-//             ['title' => 'U.S news 3', 'author' => 'Ethel', 'body' => 'some content here 3'],
-//             ['title' => 'U.S news 4', 'author' => 'Kaur', 'body' => 'some content here 4'],
-//         ];
-//         include 'views/us.php';
-//         break;
-//     default:
-//         echo '404';
-// }   
+$router = new App\Router($_SERVER['REQUEST_URI']);
+$match = $router->match();
+if($match){
+    if(is_callable($match['action'])) {
+        call_user_func($match['action']);
+    } else if (is_array($match['action'])) {
+        $className = $match['action'][0];
+        $methodName = $match['action'][1];
+        $controller = new $className();
+        $controller->$methodName();
+    } else {
+        echo 'error';
+    }
+} else {
+    echo '404';
+}
